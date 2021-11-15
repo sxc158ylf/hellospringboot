11adf4c3820bd73e6258db9948b1db9762a87b17
xuecheng <sunxuecheng6@126.com>
2021-11-15 16:26:57 +0800

    2 commit

--- src/main/java/com/xuecheng/controller/HelloSpringBoot.java
+++ src/main/java/com/xuecheng/controller/HelloSpringBoot.java
@@ -1,8 +1,6 @@
 package com.xuecheng.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()  // == rest风格 + @ResponseBody @Controller
